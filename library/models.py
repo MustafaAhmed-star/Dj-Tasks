06dfdb588545e3f97dc4aefb651cd4c8ad7f64e4
Mustafa  Abdelmgeed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-11-18 21:27:41 +0200

    design Book model

--- library/models.py
+++ library/models.py
@@ -4,3 +4,16 @@ class Author(models.Model):
     name = models.CharField(max_length=100)
     birth_date = models.DateField()
     biography = models.TextField(max_length=2000)
+    
+    
+    def __str__(self):
+        return self.name
+class Book(models.Model):
+    title = models.CharField(max_length=200) 
+    author =models.ForeignKey(Author, on_delete=models.CASCADE)
+    publication_date = models.DateField(auto_now_add=True)
+    price = models.DecimalField(max_digits=5,decimal_places=2)
+    
+    def __str__(self):
+        return f'{self.title}:{self.author}'
+        
