fa1fef58694f76edeca31182d46bc0bf03413eb2
Mustafa  Abdelmgeed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-11-18 21:21:44 +0200

    design Author model

--- library/models.py
+++ library/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    birth_date = models.DateField()
+    biography = models.TextField(max_length=2000)
