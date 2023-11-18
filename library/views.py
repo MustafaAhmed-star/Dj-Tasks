bbedb1ee133521e72f536be4b72c676066984c00
Mustafa  Abdelmgeed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-11-18 21:48:07 +0200

    create Listview and it's url

--- library/views.py
+++ library/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 from .models import Author ,Book ,Review
+from django.views.generic import CreateView, ListView, DetailView,UpdateView, DeleteView
+
 # Create your views here.
+class BookListView(ListView):
+    model = Book
+    template_name = 'library/book_list.html'
+    context_object_name = 'books'
