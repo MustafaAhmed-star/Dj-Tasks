995035c4b9a828e3781e9247defc9f961886a151
Mustafa  Abdelmgeed <67156516+MustafaAhmed-star@users.noreply.github.com>
2024-02-13 14:41:20 +0200

    add : comment form&view

--- Siebel/views.py
+++ Siebel/views.py
@@ -1,6 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
-# Create your views hede
+from .forms import CommentForm
+from .models import Post,Comment
+ 
 
-def show(request):
-    return render(request, 'post_details.html')
+# def show(request):
+#     return render(request, 'post_details.html')
+    
+    
+    
+def addComment(request,pk):
+    post = Post.objects.get(pk=pk)
+    
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = request.user
+            comment.post = post 
+            comment.save()
+            
+            return redirect(f'posts/{post.pk}')
+    
+    else:
+        form = CommentForm()
+        
+    return render(request, 'post_details.html', {'post':post,'form': form})
+        
